41d90ac9d7a89333fea9c3fb209c56bacedcbe6d
rafaelmacielrsm <rafaelmacielrsm@01b2ca82-04f9-4862-aeaf-bfd2710efad8>
2014-11-16 21:21:46 +0000

    Controles e algumas views para user

--- trunk/note2b_net/app/models/user.rb
+++ trunk/note2b_net/app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 	validates_presence_of :email, :full_name, :cpf, :password
+	validates_confirmation_of :password
+	validates_uniqueness_of :cpf
+
+	validate :email_format
+
+	private
+	def email_format
+		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+	end
 end
