df4ef102dec3945d2af4eaeb1bfa2d2e0f3f1117
rafaelmacielrsm <rafaelmacielrsm@01b2ca82-04f9-4862-aeaf-bfd2710efad8>
2014-11-22 19:54:05 +0000

    - Dependencias do tesseract e view primária pros testes iniciais

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :bills
-
   resources :users
+  get 'ocr/index'
+  get 'ocr/index2'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
