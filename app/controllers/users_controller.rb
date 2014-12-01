c69d987b8fdb143dec7a36f06f1ee9c32e1793db
rafaelmacielrsm <rafaelmacielrsm@01b2ca82-04f9-4862-aeaf-bfd2710efad8>
2014-12-01 13:16:11 +0000

    Permissões das ações do usuário

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_action :can_change, only: [:edit, :update]
+	before_action :require_no_authentication, only: [:new, :create]
+
 	def new
 		@user = User.new
 	end
@@ -34,5 +37,15 @@ class UsersController < ApplicationController
 	private
 	def user_params
 		params.require(:user).permit(:full_name, :email, :cpf, :password, :password_confirmation)
+	end
+
+	def can_change
+		unless user_signed_in? && current_user == user
+			redirect_to user_path(params[:id])
+		end
+	end
+
+	def user
+		@user ||= User.find(params[:id])
 	end
 end
